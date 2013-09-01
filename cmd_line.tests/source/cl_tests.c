0c4537f2584012c8b51bda4a2edc74840f5260ca
Ken Yourek <ken@yourek.net>
2013-09-01 14:43:50 -0500

    Re-did the main method.

--- cmd_line.tests/source/cl_tests.c
+++ cmd_line.tests/source/cl_tests.c
@@ -17,6 +17,8 @@
 #include "cl_cmd_line_opt_tests.h"
 #include "cl_cmd_line_tests.h"
 
+cl_tests *cl_tests_running_instance = NULL;
+
 static void default_print(const char *str) {
 #ifdef CL_TESTS_PRINT_PRINTF
     printf("%s", str);
@@ -111,6 +113,17 @@ void cl_tests_set_exit(cl_tests *p, cl_tests_exit_func *value) {
 }
 
 CL_TESTS_ERR cl_tests_run(cl_tests *p) {
+    if (NULL == p) return -1;
+
+    p->run_count = 0;
+    p->run_group_count = 0;
+    p->assertions_made = 0;
+    p->group_run_count = 0;
+    p->group_assertions_made = 0;
+    p->err_returned = CL_TESTS_NO_ERR;
+
+    cl_tests_running_instance = p;
+
     CL_TESTS_RUN_GROUP(cl_arg_opt_tests);
     CL_TESTS_RUN_GROUP(cl_arg_tok_owner_tests);
     CL_TESTS_RUN_GROUP(cl_arg_tok_tests);
@@ -124,5 +137,40 @@ CL_TESTS_ERR cl_tests_run(cl_tests *p) {
     CL_TESTS_RUN_GROUP(cl_cmd_line_app_tests);
     CL_TESTS_RUN_GROUP(cl_cmd_line_opt_tests);
     CL_TESTS_RUN_GROUP(cl_cmd_line_tests);
-    return CL_TESTS_NO_ERR;
+
+    return p->err_returned;
+}
+
+CL_TESTS_ERR cl_tests_main(cl_tests *p) {
+    char str[50];
+    cl_tests_exit_func *exit = cl_tests_get_exit(p);
+    cl_tests_print_func *print = cl_tests_get_print(p);
+
+    CL_TESTS_ERR err = cl_tests_run(p);
+
+    if (NULL != print) {
+        if (err) {
+            sprintf(str, "Test failed in group %d.\r\n", cl_tests_get_run_group_count(p));
+            print(str);
+        }
+        else {
+            sprintf(str, "%s", "All tests passed.\r\n");
+            print(str);
+        }
+        sprintf(str, "Tests run: %d\r\n", cl_tests_get_run_count(p));
+        print(str);
+        sprintf(str, "Assertions made: %d\r\n", cl_tests_get_assertions_made(p));
+        print(str);
+
+        if (!cl_tests_get_unattended(p)) {
+            if (NULL != exit) {
+                print("Exit?");
+                while (!exit()) {
+                    continue;
+                }
+            }
+        }
+    }
+
+    return err;
 }
