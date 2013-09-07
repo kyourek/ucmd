173018a71442151ec35784d0208f6232b6081605
Ken Yourek <kyourek@gmail.com>
2013-09-07 10:43:54 -0500

    Added a combined header for the tests project and changed the default command sizes.

--- cmd_line/include/cl_common.h
+++ cmd_line/include/cl_common.h
@@ -4,22 +4,22 @@
 /* Definitions for command, argument, and
    token sizes. */
 #ifndef CL_CMD_LEN_MAX
-#define CL_CMD_LEN_MAX                  1000
+#define CL_CMD_LEN_MAX                  200
 #endif
 #ifndef CL_TOK_LEN_MAX
 #define CL_TOK_LEN_MAX                  CL_CMD_LEN_MAX
 #endif
 #ifndef CL_CMD_COUNT_MAX
-#define CL_CMD_COUNT_MAX                20
+#define CL_CMD_COUNT_MAX                5
 #endif
 #ifndef CL_ARG_COUNT_MAX
 #define CL_ARG_COUNT_MAX                5
 #endif
 #ifndef CL_SWITCH_COUNT_MAX
-#define CL_SWITCH_COUNT_MAX             10
+#define CL_SWITCH_COUNT_MAX             5
 #endif
 #ifndef CL_RESPONSE_LEN_MAX
-#define CL_RESPONSE_LEN_MAX             CL_CMD_LEN_MAX
+#define CL_RESPONSE_LEN_MAX             1000
 #endif
 
 /* Prepend extern "C" if we're in a C++
