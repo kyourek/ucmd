49d51605c2dd4eed8eb0f53d7f84dfb5dddc7ced
Ken Yourek <kyourek@gmail.com>
2013-09-07 00:24:04 -0500

    Reformatted comments.

--- cmd_line/include/cl_common.h
+++ cmd_line/include/cl_common.h
@@ -1,10 +1,8 @@
 #ifndef CL_COMMON_H
 #define CL_COMMON_H
 
-/*
- * Definitions for command, argument, and
- * token sizes.
- */
+/* Definitions for command, argument, and
+ * token sizes. */
 #ifndef CL_CMD_LEN_MAX
 #define CL_CMD_LEN_MAX                  1000
 #endif
@@ -24,22 +22,18 @@
 #define CL_RESPONSE_LEN_MAX             CL_CMD_LEN_MAX
 #endif
 
-/*
- * Prepend extern "C" if we're in a C++
- * compiler
- */
+/* Prepend extern "C" if we're in a C++
+ * compiler. */
 #ifdef __cplusplus
 #define CL_EXTERN_C extern "C"
 #else
 #define CL_EXTERN_C
 #endif
 
-/*
- * When compiling on Windows, CL_IMPORT can
+/* When compiling on Windows, CL_IMPORT can
  * be used when using this header file in
  * another program. CL_EXPORT should be defined 
- * when compiling this program itself.
- */
+ * when compiling this program itself. */
 #ifdef CL_WIN_API
 #ifdef CL_IMPORT
 #define CL_EXPORTED CL_EXTERN_C __declspec(dllimport)
@@ -50,25 +44,19 @@
 #endif
 #endif
 
-/*
- * Default to setting CL_EXPORTED to the result
- * of our extern "C" check.
- */
+/* Default to setting CL_EXPORTED to the result
+ * of our extern "C" check. */
 #ifndef CL_EXPORTED
 #define CL_EXPORTED CL_EXTERN_C
 #endif
 
-/*
- * Definitions for a boolean type.
- */
+/* Definitions for a boolean type. */
 #define CL_BOOL     int
 #define CL_FALSE    0
 #define CL_TRUE     !CL_FALSE
 
-/*
- * Definition for the type returned
- * by functions that use an error code.
- */
+/* Definition for the type returned
+ * by functions that use an error code. */
 #define CL_ERR      int
 
 #endif
