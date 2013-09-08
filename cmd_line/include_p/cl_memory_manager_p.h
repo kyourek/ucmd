295f195da548d331ed51fede8a27870cfc301859
Ken Yourek <kyourek@gmail.com>
2013-09-08 13:06:18 -0500

    Removed type names from memory manager functions because they're not necessary.

--- cmd_line/include_p/cl_memory_manager_p.h
+++ cmd_line/include_p/cl_memory_manager_p.h
@@ -2,18 +2,18 @@
 #define CL_MEMORY_MANAGER_P_H
 
 #define CL_MEMORY_MANAGER_INIT(type, count) \
-    typedef struct cl_memory_manager_##type_instance { \
+    typedef struct cl_memory_manager_instance { \
         type s; \
         int in_use; \
-    } cl_memory_manager_##type_instance; \
-    \
-    static cl_memory_manager_##type_instance cl_memory_manager_##type_instances[count]; \
-    \
-    static type *cl_memory_manager_create_##type(void) { \
+    } cl_memory_manager_instance; \
+\
+    static cl_memory_manager_instance cl_memory_manager_instances[count]; \
+\
+    static type *cl_memory_manager_create(void) { \
         int i; \
-        cl_memory_manager_##type_instance *inst; \
+        cl_memory_manager_instance *inst; \
         for (i = 0; i < count; i++) { \
-            inst = &cl_memory_manager_##type_instances[i]; \
+            inst = &cl_memory_manager_instances[i]; \
             if (!inst->in_use) { \
                 inst->in_use = 1; \
                 return &inst->s; \
@@ -21,12 +21,12 @@
         } \
         return NULL; \
     } \
-    \
-    static void cl_memory_manager_destroy_##type(type *p) { \
+\
+    static void cl_memory_manager_destroy(type *p) { \
         int i; \
-        cl_memory_manager_##type_instance *inst; \
+        cl_memory_manager_instance *inst; \
         for (i = 0; i < count; i++) { \
-            inst = &cl_memory_manager_##type_instances[i]; \
+            inst = &cl_memory_manager_instances[i]; \
             if (p == (&inst->s)) { \
                 inst->in_use = 0; \
             } \
