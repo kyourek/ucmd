2642b81765b12135f0cf5bb766358390369702f6
Ken Yourek <kyourek@gmail.com>
2013-10-03 08:13:22 -0500

    Changed the CL_ERR define to a type.

--- cmd_line/include/cl_err.h
+++ cmd_line/include/cl_err.h
@@ -1,8 +1,17 @@
 #ifndef CL_ERR_H
 #define CL_ERR_H
 
-/* Definition for the type returned
-   by functions that use an error code. */
-#define CL_ERR      int
+/* 
+ * Summary:
+ *   Definition for the type returned
+ *   by functions that use an error code. 
+ */
+typedef int cl_err;
+
+/* 
+ * Summary:
+ *   The value that represents no error.
+ */
+#define CL_ERR_NONE 0
 
 #endif
