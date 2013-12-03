72e48edc927ef2a7da9c07a5f7090c82ca62a935
Ken Yourek <kyourek@gmail.com>
2013-12-03 15:33:53 -0600

    Included default configs for console and embedded.

---
+++ ucmd/config/console/default/uc_console.h
@@ -0,0 +1,7 @@
+#ifndef UC_CONSOLE_H
+#define UC_CONSOLE_H
+   
+/* This file is left blank for compiling with default
+   console configuration. */
+   
+#endif
--- ucmd/config/embedded/uc_config.h
+++ ucmd/config/embedded/uc_config.h
@@ -1,4 +1,8 @@
 #ifndef UC_CONFIG_H
 #define UC_CONFIG_H
 
+/* Each embedded implementation should
+   provide its own configuration file. */
+#include "uc_embedded.h"
+
 #endif
