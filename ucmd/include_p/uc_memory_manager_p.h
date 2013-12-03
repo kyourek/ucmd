5b582957924509b486928971785b23d1d48ea880
Ken Yourek <kyourek@gmail.com>
2013-12-03 14:49:44 -0600

    Changed case of macro parameters.

--- ucmd/include_p/uc_memory_manager_p.h
+++ ucmd/include_p/uc_memory_manager_p.h
@@ -1,18 +1,18 @@
 #ifndef UC_MEMORY_MANAGER_P_H
 #define UC_MEMORY_MANAGER_P_H
 
-#define UC_MEMORY_MANAGER_INIT(type, count) \
+#define UC_MEMORY_MANAGER_INIT(TYPE, COUNT) \
     typedef struct uc_memory_manager_instance { \
-        type s; \
+        TYPE s; \
         char in_use; \
     } uc_memory_manager_instance; \
 \
-    static uc_memory_manager_instance uc_memory_manager_instances[count]; \
+    static uc_memory_manager_instance uc_memory_manager_instances[COUNT]; \
 \
-    static type *uc_memory_manager_create(void) { \
+    static TYPE *uc_memory_manager_create(void) { \
         int i; \
         uc_memory_manager_instance *inst; \
-        for (i = 0; i < count; i++) { \
+        for (i = 0; i < COUNT; i++) { \
             inst = &uc_memory_manager_instances[i]; \
             if (!inst->in_use) { \
                 inst->in_use = 1; \
@@ -22,10 +22,10 @@
         return NULL; \
     } \
 \
-    static void uc_memory_manager_destroy(type *p) { \
+    static void uc_memory_manager_destroy(TYPE *p) { \
         int i; \
         uc_memory_manager_instance *inst; \
-        for (i = 0; i < count; i++) { \
+        for (i = 0; i < COUNT; i++) { \
             inst = &uc_memory_manager_instances[i]; \
             if (p == (&inst->s)) { \
                 inst->in_use = 0; \
