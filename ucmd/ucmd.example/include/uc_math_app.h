bb77eb19041310a705db6d270f8673243c42327e
Ken Yourek <ken@yourek.net>
2014-02-13 14:32:52 -0600

    Removed old example files.

--- ucmd/ucmd.example/include/uc_math_app.h
+++
@@ -1,8 +0,0 @@
-#ifndef UC_MATH_APP_H
-#define UC_MATH_APP_H
-
-#include "uc_app.h"
-
-UC_EXPORTED uc_app *uc_math_app_get_instance(void);
-
-#endif
--- ucmd/ucmd.example/include/ucmd_example.h
+++ ucmd/ucmd.example/include/ucmd_example.h
@@ -1,6 +1,6 @@
 #ifndef CMD_LINE_EXAMPLE_H
 #define CMD_LINE_EXAMPLE_H
 
-#include "uc_math_app.h"
+#include "uc_hello_world.h"
 
 #endif
