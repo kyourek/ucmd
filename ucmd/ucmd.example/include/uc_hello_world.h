3ea473331574f11baacb82e3359a6e31cbfd7a1a
Ken Yourek <ken@yourek.net>
2014-02-13 16:46:53 -0600

    Added another example.

--- ucmd/ucmd.example/include/uc_hello_world.h
+++ ucmd/ucmd.example/include/uc_hello_world.h
@@ -1,7 +1,6 @@
 #ifndef UC_HELLO_WORLD_H
 #define UC_HELLO_WORLD_H
 
-#include "uc_common.h"
 #include "uc_cmd_line_app.h"
 
 UC_EXPORTED void uc_hello_world(uc_cmd_line_transmit_func *transmit, uc_cmd_line_app_receive_func *receive);
