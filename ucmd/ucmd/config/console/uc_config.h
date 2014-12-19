d64eb275c6c29b45b9dc9294164baf1b4775abc6
Ken Yourek <ken@yourek.net>
2014-12-19 14:36:17 -0600

    Remove old configuration define files.

--- ucmd/ucmd/config/console/uc_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef UC_CONFIG_H
-#define UC_CONFIG_H
-
-/* Each console implementation should
-   provide its own configuration file. */
-#include "uc_console.h"
-
-#endif
--- ucmd/ucmd/config/embedded/uc_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef UC_CONFIG_H
-#define UC_CONFIG_H
-
-/* Each embedded implementation should
-   provide its own configuration file. */
-#include "uc_embedded.h"
-
-#endif
