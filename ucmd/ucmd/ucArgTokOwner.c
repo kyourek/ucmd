d3c5c7b38025ca64a7d44c93f7af2857afd02371
Ken Yourek <ken@yourek.net>
2015-11-04 16:18:54 -0600

    Remove unnecessary casts.

--- ucmd/ucmd/ucArgTokOwner.c
+++ ucmd/ucmd/ucArgTokOwner.c
@@ -2,8 +2,8 @@
 
 ucArgTok *ucArgTokOwner_get_arg(ucArgTokOwner *p) {
     ucTok* tok;
-    tok = ucTok_get_next((ucTok*)p);
+    tok = ucTok_get_next(p);
     if (!tok) return NULL;
     if (ucTok_is_switch(tok)) return NULL;
-    return (ucArgTok*)tok;
+    return tok;
 }
